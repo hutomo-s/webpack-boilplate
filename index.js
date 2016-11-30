ef6f78ba2f474ce29842dbe62d04949bb0d83e03
Hutomo Sugianto <hutomo@kudo.co.id>
2016-11-30 10:10:05 +0700

    Modify index.js

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Dropdown, Button} from 'react-materialize';
+import {NavItem, Navbar, Dropdown, Button, Icon} from 'react-materialize';
 
-const App = ({name}) =>
-  <Dropdown trigger={
-    <Button>Drop me!</Button>
-  }>
-    <p>Hello</p>
-    <p>Hello</p>
-    <p>Hello</p>
-    <p>Bye</p>
-  </Dropdown>;
+class Nav extends React.Component {
+  render() {
+    return <Navbar brand='logo' right>
+      <NavItem href='get-started.html'>Getting started</NavItem>
+      <NavItem href='components.html'>Components</NavItem>
+    </Navbar>;
+  }
+}
+
+class Dd extends React.Component {
+  render() {
+    return <Dropdown trigger={
+        <Button>Drop me!</Button>
+      }>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Bye</p>
+    </Dropdown>;
+    }
+}
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <Nav />
+        <Dd />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />, document.querySelector('.react-root')
