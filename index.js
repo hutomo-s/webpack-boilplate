6d5dff0f2a6128a47a999c3574beaf826f8e7a4e
Hutomo Sugianto <hutomo@kudo.co.id>
2016-12-03 09:44:47 +0700

    footer in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {NavItem, Navbar, Dropdown, Button, Icon} from 'react-materialize';
+import {NavItem, Navbar, Dropdown, Button, Icon, Footer} from 'react-materialize';
 
 class Nav extends React.Component {
   render() {
@@ -37,6 +37,30 @@ class Paralax extends React.Component {
   }
 }
 
+class Foot extends React.Component {
+  render() {
+    return (
+      <Footer copyrights="&copy; 2015 Copyright Text"
+        moreLinks={
+          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+        }
+        links={
+          <ul>
+            <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+            <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+            <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+            <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+          </ul>
+        }
+        className='example'
+      >
+          <h5 className="white-text">Footer Content</h5>
+          <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+      </Footer>
+    );
+  }
+}
+
 class Dd extends React.Component {
   render() {
     return (
@@ -60,7 +84,7 @@ class App extends React.Component {
       <div>
         <Nav />
         <Paralax />
-        
+        <Foot />
       </div>
     );
   }
