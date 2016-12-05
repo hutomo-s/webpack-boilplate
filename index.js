87257a4d417a18e24e018e4a916a15b9c302b963
Hutomo Sugianto <hutomo@kudo.co.id>
2016-12-05 07:17:14 +0700

    change footer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {NavItem, Navbar, Dropdown, Button, Icon, Footer} from 'react-materialize';
+import {NavItem, Navbar, Dropdown, Button, Icon, Footer, Col} from 'react-materialize';
 
 class Nav extends React.Component {
   render() {
@@ -37,26 +37,67 @@ class Paralax extends React.Component {
   }
 }
 
-class Foot extends React.Component {
+const ptop0 = {
+  'padding-top': '0px',
+};
+
+class FootLinks extends React.Component {
   render() {
     return (
-      <Footer copyrights="&copy; 2015 Copyright Text"
-        moreLinks={
-          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-        }
-        links={
-          <ul>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-          </ul>
-        }
-        className='example'
-      >
-          <h5 className="white-text">Footer Content</h5>
-          <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-      </Footer>
+
+      <div className="blue-grey white-text">
+
+        <div className="row container section">
+          
+          <Col s={12} m={6} l={6} className='grid-example'>
+            <h5 className="white-text">Popular Culinary Destination</h5>
+            <ul>
+              <li><a className="grey-text text-lighten-3" href="#!">Slipi, DKI Jakarta Culinary</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Cihampelas, Bandung Culinary</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Tj Pandan, Belitung Culinary</a></li>
+            </ul>
+          </Col>
+
+          <Col s={12} m={6} l={6} className='grid-example'>
+            <h5 className="white-text">Find Nearest ATM</h5>
+            <ul>
+              <li><a className="grey-text text-lighten-3" href="#!">Slipi, DKI Jakarta ATMs</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Cihampelas, Bandung ATMs</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Tj Pandan, Belitung ATMs</a></li>
+            </ul>
+          </Col>
+
+          <Col s={12} m={6} l={6} className='grid-example'>
+            <h5 className="white-text">Need Groceries?</h5>
+            <ul>
+              <li><a className="grey-text text-lighten-3" href="#!">Slipi, DKI Jakarta Minimarkets</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Cihampelas, Bandung Minimarkets</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Tj Pandan, Belitung Minimarkets</a></li>
+              
+            </ul>
+          </Col>
+
+          <Col s={6} m={3} l={3} className='grid-example'>
+            <h5 className="white-text">About TraveLink</h5>
+            <ul>
+              <li><a className="grey-text text-lighten-3" href="#!">Why use TraveLink</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Contact Us</a></li>
+            </ul>
+          </Col>
+        </div>
+
+        <footer className="page-footer blue-grey darken-2" style={ptop0}>
+          <div className="footer-copyright">
+            <div className="container">
+              <span>Copyright © 2016 <a class="grey-text text-lighten-4">Perdana</a> All rights reserved.</span>
+              <span className="right"> Design and Developed by <a className="grey-text text-lighten-4">PerdanaLabs</a></span>
+              </div>
+          </div>
+        </footer>
+
+      </div>
+
+     
     );
   }
 }
@@ -84,7 +125,7 @@ class App extends React.Component {
       <div>
         <Nav />
         <Paralax />
-        <Foot />
+        <FootLinks />
       </div>
     );
   }
